33b5975dd6e8edb59a3c4a5217d9ea5a3c5ed3c5
Ommarico <94463501+Ommarico@users.noreply.github.com>
2021-12-05 00:52:53 +0800

    Update AssemblyInfo.cs

--- Agent.Installer.Win/Properties/AssemblyInfo.cs
+++ Agent.Installer.Win/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Remotely Installer")]
-[assembly: AssemblyDescription("An installer for the Remotely service, which provides unattended remote access and remote scripting.")]
+[assembly: AssemblyTitle("Remoteme Installer")]
+[assembly: AssemblyDescription("An installer for the Remoteme service, which provides unattended remote access and remote scripting.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Translucency Software")]
-[assembly: AssemblyProduct("Remotely Installer")]
-[assembly: AssemblyCopyright("Copyright ©  2020 Translucency Software")]
+[assembly: AssemblyCompany("Uniten")]
+[assembly: AssemblyProduct("Remoteme Installer")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Uniten")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
